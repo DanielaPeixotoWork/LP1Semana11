fb01144fe8cd706f4729038514efbff9fa78f832
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-05-22 12:29:14 +0100

    Adição da classe, foram feitas três variáveis, contrutor vazio que inicia as variáveis, método para obter o item

---
+++ MyGenericClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyGenericClass
+{
+
+    public class Guarda3<T>
+    {
+        private T _item1;
+        private T _item2;
+        private T _item3;
+
+        public Guarda3()
+        {
+            _item1 = default(T);
+            _item2 = default(T);
+            _item3 = default(T);
+        }
+
+        public T GetItem(int i)
+        {
+
+            if (i < 0 || i > 2)
+                throw new IndexOutOfRangeException("Index must be 0, 1, or 2.");
+
+            return i switch
+            {
+                0 => _item1,
+                1 => _item2,
+                2 => _item3,
+                _ => throw new IndexOutOfRangeException("Index must be 0, 1, or 2."),
+            };
+        }
+    }
+}
