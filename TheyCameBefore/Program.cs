0e69cd0a9902f40b26fef2b64cc4899cb2142919
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-05-22 12:21:24 +0100

    Criaçao da classe program que contém o método main onde testa a lista de inteiros

--- TheyCameBefore/Program.cs
+++ TheyCameBefore/Program.cs
@@ -11,4 +11,36 @@ namespace TheyCameBefore
             return items.Where(item => item.CompareTo(comparisonItem) < 0);
         }
     }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intItems = new List<int> { 1, 2, 3, 4, 5, 6 };
+            int intComparisonItem = 4;
+            var intResult = BeforeUtils.GetTheOnesBefore(intItems, intComparisonItem);
+
+            Console.WriteLine("Integers before 4:");
+            foreach (var item in intResult)
+            {
+                Console.WriteLine(item);
+            }
+
+            var dateItems = new List<DateTime>
+            {
+                new DateTime(2023, 1, 1),
+                new DateTime(2023, 6, 15),
+                new DateTime(2024, 1, 1),
+                new DateTime(2022, 12, 31)
+            };
+            DateTime dateComparisonItem = new DateTime(2023, 1, 1);
+            var dateResult = BeforeUtils.GetTheOnesBefore(dateItems, dateComparisonItem);
+
+            Console.WriteLine("\nDates before January 1, 2023:");
+            foreach (var date in dateResult)
+            {
+                Console.WriteLine(date.ToShortDateString());
+            }
+        }
+    }
 }
