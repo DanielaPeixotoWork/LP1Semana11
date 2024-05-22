e8ff32d7a79ced58de7fc75fc4b5bb3ca9d477ed
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-05-22 12:13:44 +0100

    Criação da classe e foi feito o método GetTheOnesBefore

---
+++ TheyCameBefore/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheyCameBefore
+{
+    public static class BeforeUtils
+    {
+        public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> items, T comparisonItem) where T : struct, IComparable<T>
+        {
+            return items.Where(item => item.CompareTo(comparisonItem) < 0);
+        }
+    }
+}
